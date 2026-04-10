320506ee04819eae7676d9ae63c3802231331559
chad <chad@skillcape.software>
2026-04-09 21:24:24 -0400

    bind Fastify to port 8420

--- api/src/index.ts
+++ api/src/index.ts
@@ -80,7 +80,7 @@ async function start() {
 
   // ── Start ──────────────────────────────────────────────────────────────────
   try {
-    await fastify.listen({ port: 3000, host: "0.0.0.0" });
+    await fastify.listen({ port: 8420, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
